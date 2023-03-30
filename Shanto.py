c0c486c19103648bfdc0784191a9a5ca926fc02b
RJ SHANTO <124341668+hridoyshanto@users.noreply.github.com>
2023-03-30 08:19:18 +0600

    Create Shanto.py

---
+++ Shanto.py
@@ -0,0 +1,15 @@
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.050)
+xoss('\033[0;91m Checking Your Mobile...?');time.sleep(0.50)
+time.sleep(10)
+xoss("\033[0;92m Congratulations Your Device Support This Tools")
+time.sleep(3)
+xoss('\033[0;93m FOLLOW MY ID')
+time.sleep(3)
+os.system('xdg-open https://www.facebook.com/rjshanto723
+import Jr
+Jr.main()
